7ac546287639715f06bf69c4d345ee617745ae5a
InKolev <in.kolev.b@gmail.com>
2016-04-28 00:06:14 +0300

    Started integrating Ninject

--- Source/Desktop/HallScheduler.Desktop.Client/Configuration/NinjectConfig.cs
+++ Source/Desktop/HallScheduler.Desktop.Client/Configuration/NinjectConfig.cs
@@ -1,22 +1,15 @@
 ﻿namespace HallScheduler.Desktop.Client.Configuration
 {
     using Ninject.Modules;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using Services;
+    using Services.Contracts;
 
     public class NinjectConfig : NinjectModule
     {
         public override void Load()
         {
-            // Create bindings
-            //Bind<IFlagsDbContext>().To<FlagsDbContext>().InSingletonScope();
-            //Bind<IGameplay>().To<GameplayViewModel>();
-            //Bind<IStartScreen>().To<StartScreenViewModel>();
-            //Bind<IScoreboard>().To<ScoreboardViewModel>();
-            //Bind<IEngine>().To<EngineViewModel>().InSingletonScope();
+            Bind<IHttpService>().To<HttpService>();
+            Bind<IIdentityService>().To<IdentityService>().InSingletonScope();
         }
     }
 }
